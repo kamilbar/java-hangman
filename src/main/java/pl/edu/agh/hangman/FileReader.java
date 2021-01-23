17a3bf322b89ff1f60fec4fd29ad617d50482f6e
Mariusz Franczyk <mariusz.franczyk@gmail.com>
2021-01-23 14:49:47 +0100

    RandomWord and FileReader are ready

--- src/main/java/pl/edu/agh/hangman/FileReader.java
+++ src/main/java/pl/edu/agh/hangman/FileReader.java
@@ -1,5 +1,27 @@
 package pl.edu.agh.hangman;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class FileReader {
 
+	public static List<String> readInputFile() throws FileNotFoundException {
+
+		File file = new File("src/main/resources/slowa.txt");
+		Scanner scanner = new Scanner(file);
+		
+		List<String> words = new ArrayList<>();
+		
+		while (scanner.hasNext()) {
+			String token = scanner.nextLine();
+
+			words.add(token);
+		}
+		scanner.close();
+		return words;
+
+	}
 }
--- src/main/java/pl/edu/agh/hangman/RandomWord.java
+++ src/main/java/pl/edu/agh/hangman/RandomWord.java
@@ -1,5 +1,20 @@
 package pl.edu.agh.hangman;
 
-public class RandomWord {
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Random;
 
+public class RandomWord {
+	
+public static String generateWord() throws FileNotFoundException {
+		
+		Random generator = new Random(); 
+		List<String> wordList = FileReader.readInputFile();
+		
+		int randomNumber = generator.nextInt(wordList.size());
+		
+		
+		return wordList.get(randomNumber);
+		
+	}
 }
